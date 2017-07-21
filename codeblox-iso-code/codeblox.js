78d3cb6f4be0cab29744c5aab3e88e5938f40b4e
Daniel Bolivar <daniel.bolivar@netcentric.biz>
2017-07-21 13:17:41 +0200

    Iso Code Finished

--- codeblox-iso-code/codeblox.js
+++ codeblox-iso-code/codeblox.js
@@ -1,4 +1,9 @@
-const codeblox = require('codeblox')
-module.exports.default = codeblox((input, options, callback) => {
+const codeblox = require('codeblox');
+const request = require('request');
+const endpoint = 'https://restcountries.eu/rest/v2/name/';
 
-})
+module.exports.default = codeblox((input, options, callback) => {
+  request.get(`${endpoint}${input}`, (err, res, body) => {
+    callback(null, JSON.parse(body)[0].alpha2Code);
+  });
+});
